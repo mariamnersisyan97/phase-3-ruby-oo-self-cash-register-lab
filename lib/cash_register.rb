698b41ef1a2eef5695d01a5ed32c2b4631a40ca6
Mariam Nersisyan <mariamnersisyan97@gmail.com>
2022-07-05 21:07:41 -0400

    commit tree 3e41554fdcff61cd0d7500fd4c8fe9841a893432

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,39 @@
+require "pry"
+class CashRegister
+    attr_reader :discount
+    attr_accessor :total, :items, :last_price
 
+    def initialize employee_discount=0
+        @total = 0
+        @discount = employee_discount
+        @items = []
+        @last_price = 0
+    end
+
+    def add_item title, price, quantity=1
+        quantity.times do 
+        self.items << title
+        # shovel in items 
+        end
+        self.last_price = price.to_f * quantity
+
+        self.total += price * quantity
+         # self.total = self.total + price
+        # These operate similarly by setting the writer method and then the reader
+    end
+
+    def apply_discount
+        if self.discount > 0
+             self.total = self.total - (self.discount.to_f / 100) * self.total
+             "After the discount, the total comes to $#{self.total.to_i}."
+        else
+             "There is no discount to apply."
+        end
+    end
+
+    def void_last_transaction
+        self.total = self.total - self.last_price
+    end
+    
+
+end
